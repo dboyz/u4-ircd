55ce46d6e3f31895be55ba198b9a406b1f0f8d0a
David Kingston <deathking1337@aim.com>
2009-08-02 14:35:01 -0400

    Commited first revision

---
+++ include/user.h
@@ -0,0 +1,51 @@
+/*  UnrealIRCd -- The Next Generation IRCd.
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 1, or (at your option)
+*   any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program; if not, write to the Free Software
+*   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+class User
+{
+ public:
+	/* The nick of the IRC user. */
+	std::string nick;
+
+	/* The ident of the IRC user. */
+	std::string ident;
+
+	/* The host of the IRC user. */
+	std::string host;
+
+	/* The GECOS (Real Name) of the IRC user. */
+	std::string gecos;
+
+	/* The IP of the IRC user. */
+	std::string ip;
+
+	/* A doubly linked list of the channels the IRC user is currently
+	*  in.
+	*/
+	std::list in_channels;
+
+	/* Is the user an oper, not taking into consideration any other
+	*  privliges?
+	*/
+	bool isOper = false;
+
+	/* Quit's a user with the specified kill message.
+	*  Parameters:
+	*  reason - The reason to quit the user.
+	*/
+	void Quit(const std::string& reason);
+};

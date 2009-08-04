b5f37ff4a47309ffc469fc221c71c0e338cbe654
Binki <ohnobinki@ohnopublishing.net>
2009-08-04 10:22:18 -0400

    enough buildsystem to get started
    To use the buildsystem (to get a ./configure), run autoreconf -vfi

---
+++ src/channels.cpp
@@ -0,0 +1,41 @@
+/*  UnrealIRCd -- The Next Generation IRCd.
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 1, or (at your option)
+*   any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program; if not, write to the Free Software
+*   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+bool Channel::isValidChannel(const std::string& channame)
+{
+	if (channame.c_str() == NULL)
+	{
+		return false;
+	}
+	else
+	{
+		MODULARIZE_FUNCTION(I_OnValidChannelCheck, OnValidChannelCheck(channame.c_str());
+		return true;
+	}
+}
+
+int Channel::KickUser(User * from, User * to, const std::string& reason)
+{
+	if (!from || !to || reason.c_str() == NULL)
+	{
+		return -1;
+	}
+	else
+	{
+		MODULARIZE_FUNCTION(I_OnUserKick, OnUserKick(from, to, reason.c_str());
+	}
+}

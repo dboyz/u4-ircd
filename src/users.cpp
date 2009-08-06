85d39efc3f9e77a75ac5efd14534be455f42a493
Stealth <stealth@x-tab.org>
2009-08-06 11:27:35 -0700

    Renamed a bunch of stuff in the SendAdmin function. SendAdmin is now known as SendAdminInfo

--- src/users.cpp
+++ src/users.cpp
@@ -118,16 +118,16 @@ void User::SendMOTD(void)
 	return;
 }
 
-void User::SendAdmin(void)
+void User::SendAdminInfo(void)
 {
-	MODULARIZE_FUNCTION(I_OnPreADMINSend, OnPreADMINSend());
+	MODULARIZE_FUNCTION(I_OnPreSendAdminInfo, OnPreSendAdminInfo());
 
 	this->SendRaw(RPL_ADMINME, conf->ServerName.c_str(), this->nick.c_str(), conf->ServerName.c_str());
 	this->SendRaw(RPL_ADMINNAME, conf->ServerName.c_str(), this->nick.c_str(), conf->AdminName.c_str());
 	this->SendRaw(RPL_ADMINNICK, conf->ServerName.c_str(), this->nick.c_str(), conf->AdminNick.c_str());
 	this->SendRaw(RPL_ADMINEMAIL, conf->ServerName.c_str(), this->nick.c_str(), conf->AdminEmail.c_str());
 
-	MODULARIZE_FUNCTION(I_OnADMINSend, OnADMINSend());
+	MODULARIZE_FUNCTION(I_OnSendAdminInfo, OnSendAdminInfo());
 
 	return;
 }

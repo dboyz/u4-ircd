3a72b85c7d53dc9cf7c021550279094c028558e7
Binki <ohnobinki@ohnopublishing.net>
2009-08-11 13:37:48 -0400

    syntactical and semantic fixes for SocketEngine

--- src/socketengine.cpp
+++ src/socketengine.cpp
@@ -17,21 +17,52 @@
 
 #include "socketengine.h"
 
-SocketEngine
-SocketEngine::create(Module& module)
+
+SocketInfo::SocketInfo(int socket_, SocketEventHandler *handler_)
+	: socket(socket_), 
+	  handler(handler_)
 {
-	SocketEngine &newEngine;
+}
+
+bool
+SocketEngine::SocketInfoRegistered(int socket)
+{
+	for(std::vector<SocketInfo>::iterator i = sockets.begin(); i != sockets.end(); i ++)
+		if(i->socket == socket)
+			return true;
+	return false;
+}
+
+SocketInfo &
+SocketEngine::getSocketInfo(int socket) throw(std::runtime_error)
+{
+	for(std::vector<SocketInfo>::iterator i = sockets.begin(); i != sockets.end(); i ++)
+		if(i->socket == socket)
+		{
+			i->handler->handleEvent(socket);
+			return *i;
+		}
+	throw std::runtime_error("socket " + ConvertIntToString(socket) + " requested but not found");
+}
+
+SocketEngine*
+SocketEngine::create(Module& module) throw(std::bad_cast)
+{
+	SocketEngine *newEngine;
 	
-	newEngine = (SocketEngine&) module->getObject("SocketEngine");
+	newEngine = dynamic_cast<SocketEngine>(module.getObject("SocketEngine"));
+	if(!newEngine)
+		throw std::bad_cast();
 	
+	return newEngine;
 }
 
 void
-SocketEngine::runEventHandler(int socket)
+SocketEngine::runEventHandler(int socket) throw(std::runtime_error)
 {
-	for(std::vector<SocketInfo>::iterator i = sockets.begin; i; i ++)
-		if(i->socket == socket)
-			runEventHandler(*i);
+	SocketInfo &sockinfo = getSocketInfo(socket);
+	
+	runEventHandler(sockinfo);
 }
 
 void
@@ -41,25 +72,27 @@ SocketEngine::runEventHandler(SocketInfo& sock)
 }
 
 void
-addSocket(int socket, SocketEventHandler *handler)
+SocketEngine::addSocket(int socket, SocketEventHandler *handler)
 {
 	/*
-	 * TODO: make sure that each socket only has one 
-	 * handler associated with it
+	 * TODO: Design decision:
+	 * Should we allow multiple SocketEventHandlers to be
+	 * registered per socket or only one? Currently, we
+	 * only allow one per socket.
 	 */
-	SocketInfo sockinfo;
-
-	sockinfo->socket = socket;
-	sockinfo->handler = handler;
+	if(SocketInfoRegistered(socket))
+		throw std::runtime_error("socket " + ConvertIntToString(socket) + " already in socketengine, attempt to readd it");
+	
+	SocketInfo sockinfo(socket, handler);
 
 	sockets.push_back(sockinfo);
 	monitorSocket(sockinfo);
 }
 
 void
-removeSocket(int socket)
+SocketEngine::removeSocket(int socket)
 {
-	for(std::vector<SocketInfo>::iterator i = sockets.begin; i; i ++)
+	for(std::vector<SocketInfo>::iterator i = sockets.begin(); i != sockets.end(); i ++)
 		if(i->socket == socket)
 		{
 			unmonitorSocket(*i);

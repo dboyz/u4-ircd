4276b25aa3579945c1b3c141a0a89dc8b29a6727
Binki <ohnobinki@ohnopublishing.net>
2009-10-16 23:55:08 -0400

    main() and IRCd class skeletons

---
+++ include/ircd.h
@@ -0,0 +1,42 @@
+/*  UnrealIRCd -- The Next Generation IRCd.
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 1, or (at your option)
+*   any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program; if not, write to the Free Software
+*   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef _UNREALIRCD_IRCD_H
+#define _UNREALIRCD_IRCD_H
+
+#include <string>
+
+/*
+  The IRCd object through which all data about the server can be accessed (mostly indirectly).
+ */
+
+class Config;
+
+class IRCd
+{
+public:
+	IRCd(std::string config_path);
+	~IRCd();
+
+	void die();
+
+	Config *conf;
+protected:
+private:
+};
+
+#endif

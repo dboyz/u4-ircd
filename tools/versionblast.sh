eb628f7c9d2ff592391f79ed6b245893abf2bbc5
Binki <ohnobinki@ohnopublishing.net>
2010-01-11 23:53:56 -0500

    handle src/version.cpp correctly for releases

--- tools/versionblast.sh
+++ tools/versionblast.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 echo "Creating ${top_builddir}/src/version.cpp"
+
+# if distributed:
+${DOCP}
+[ -z "${DOCP}" ] || exit 0
+
+# if developed:
 ${MKDIR_P} ${top_builddir}/src
 ${SED} -e s/@PACKAGE_CHANGESET@/\"$(hg -R "${top_srcdir}" id -i)\"/ ${top_srcdir}/src/version.cpp.in > ${top_builddir}/src/version.cpp

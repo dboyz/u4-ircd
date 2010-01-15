d63b800335fc9185cb6f48c3b7e2d9cd5c7b55f3
commx <commx@commx.ws>
2010-01-15 02:48:02 +0100

    adding rehash command

---
+++ src/cmd/rehash.cpp
@@ -0,0 +1,92 @@
+/*****************************************************************
+ * Unreal Internet Relay Chat Daemon, Version 4
+ * File         rehash.cpp
+ * Description  REHASH command handler
+ *
+ * Copyright(C) 2009, 2010
+ * The UnrealIRCd development team and contributors
+ * http://www.unrealircd.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ ******************************************************************/
+
+#include "base.hpp"
+#include "cmdmap.hpp"
+#include "command.hpp"
+#include "module.hpp"
+#include "stringlist.hpp"
+
+/** Module informations */
+UnrealModule::Info modinf =
+{
+	/** Module name */
+	"REHASH command handler",
+
+	/** Module version */
+	"1.0",
+
+	/** Module author */
+	"(Packaged)"
+};
+
+/** Command Instance */
+UnrealUserCommand* uc = 0;
+
+/**
+ * REHASH command handler for User connections.
+ *
+ * Usage:
+ * REHASH
+ *
+ * @param uptr Originating user
+ * @param argv Argument list
+ */
+void uc_rehash(UnrealUser* uptr, StringList* argv)
+{
+	unreal->config.rehash();
+
+	uptr->sendreply(RPL_REHASHING,
+		String::format(MSG_REHASHING,
+			unreal->config.fileName().c_str()));
+}
+
+/**
+ * Module initialization function.
+ * Called when the Module is loaded.
+ *
+ * @param module Reference to Module
+ */
+UNREAL_DLL UnrealModule::Result unrInit(UnrealModule& module)
+{
+	/* update module info */
+	module.info = modinf;
+
+	/* register command */
+	uc = new UnrealUserCommand(CMD_REHASH, &uc_rehash, true);
+
+	return UnrealModule::Success;
+}
+
+/**
+ * Module close function.
+ * It's called before the Module is unloaded.
+ */
+UNREAL_DLL UnrealModule::Result unrClose(UnrealModule& module)
+{
+	delete uc;
+
+	return UnrealModule::Success;
+}
